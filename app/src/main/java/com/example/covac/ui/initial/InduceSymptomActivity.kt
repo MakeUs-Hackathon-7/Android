e93ba219133964656421c5303dd84978bae214fd
taeeunn <xodms8713@gmail.com>
2021-03-13 02:43:35 +0900

    feat: 증상 유도 페이지

---
+++ app/src/main/java/com/example/covac/ui/initial/InduceSymptomActivity.kt
@@ -0,0 +1,12 @@
+package com.example.covac.ui.initial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.covac.R
+
+class InduceSymptomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_induce_symptom)
+    }
+}
